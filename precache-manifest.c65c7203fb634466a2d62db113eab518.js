77ba12a150f403a43923a492199fc3ded95e1539
ayesha omarali <ayesha.omarali@gmail.com>
2019-03-10 19:31:33 -0700

    Updates

---
+++ precache-manifest.c65c7203fb634466a2d62db113eab518.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "af9e86425f4c69a86a5a",
+    "url": "/static/css/main.0ae822e8.chunk.css"
+  },
+  {
+    "revision": "af9e86425f4c69a86a5a",
+    "url": "/static/js/main.af9e8642.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "79bf9289b0016558e9d2",
+    "url": "/static/js/2.79bf9289.chunk.js"
+  },
+  {
+    "revision": "e96405d35b2eb209c81018bcfc6b5f1e",
+    "url": "/static/media/me_scorpion.e96405d3.jpeg"
+  },
+  {
+    "revision": "9aa2ade9226a20d6c33c0de959361e03",
+    "url": "/static/media/resume.9aa2ade9.pdf"
+  },
+  {
+    "revision": "88f76854c68d34c185e4883091addc77",
+    "url": "/static/media/me.88f76854.jpg"
+  },
+  {
+    "revision": "ae5acabbdcd7d5df64a53ff7b3983883",
+    "url": "/index.html"
+  }
+];
